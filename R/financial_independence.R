223d912b2f0be0aa6a82b7e076a7093ffb542699
ananthu1834 <ananthu@systeminsights.com>
2019-10-05 07:11:39 +0000

    refactoring

--- R/financial_independence.R
+++ R/financial_independence.R
@@ -1,14 +1,24 @@
 
-expense_future <- function(current_monthly_expense, years, currency_inflation, lifestyle_inflation) {
-  currency_inflation_factor = (1+(0.01*currency_inflation))^years
-  lifestyle_inflation_factor = (1+(0.01*lifestyle_inflation))^years
-  current_monthly_expense*currency_inflation_factor*lifestyle_inflation_factor
+.compounding_factor <- function(rate, years) (1+(0.01*rate))^years
+
+.currency_inflation_factor <- function(currency_inflation, years) .compounding_factor(currency_inflation, years)
+.lifestyle_inflation_factor <- function(lifestyle_inflation, years) .compounding_factor(lifestyle_inflation, years)
+.mf_growth_factor <- function(mf_growth, years) .compounding_factor(mf_growth, years)
+
+.mf_growth_factor_for_sip <- function(mf_growth, years, sip_growth = 0) {
+  total = 0
+  for(i in 1:years) total = total + ((1+0.01*sip_growth)^(years-i))*(1+0.01*mf_growth)^i
+  total
+}
+
+future_expense <- function(current_expense, years, currency_inflation, lifestyle_inflation) {
+  current_expense * .currency_inflation_factor(currency_inflation, years) * .lifestyle_inflation_factor(lifestyle_inflation, years)
 }
 
 total_expenses_for_period <- function(current_monthly_expense, start_year, withdrawal_period, currency_inflation, lifestyle_inflation) {
   years_of_period = start_year:(start_year + withdrawal_period)
   total_expenses = 0
-  for (i in years_of_period) total_expenses = total_expenses + 12*expense_future(current_monthly_expense, i, currency_inflation, lifestyle_inflation)
+  for (i in years_of_period) total_expenses = total_expenses + 12*future_expense(current_monthly_expense, i, currency_inflation, lifestyle_inflation)
   total_expenses
 }
 
@@ -19,7 +29,7 @@ total_savings_for_period <- function(current_monthly_expense, start_year, withdr
   total_expenses_next_period = total_expenses_for_period(current_monthly_expense, start_year + withdrawal_period, withdrawal_period, currency_inflation, lifestyle_inflation)
   total_savings_next_period = total_savings_for_period(current_monthly_expense, start_year + withdrawal_period, withdrawal_period, currency_inflation, lifestyle_inflation, mf_growth, life_expectancy, age_current)
 
-  mf_growth_factor = .mf_growth_factor_for_lumpsum(withdrawal_period, mf_growth)
+  mf_growth_factor = .mf_growth_factor(mf_growth, withdrawal_period)
   (total_expenses_next_period + total_savings_next_period)/mf_growth_factor
 }
 
@@ -30,24 +40,18 @@ lumpsum_needed_for_ind <- function(current_monthly_expense, start_year, withdraw
   total_savings_start_period + total_expenses_start_period
 }
 
-.mf_growth_factor_for_sip <- function(years, sip_growth = 0, mf_growth) {
-  total = 0
-  for(i in 1:years) total = total + ((1+0.01*sip_growth)^(years-i))*(1+0.01*mf_growth)^i
-  total
-}
-
-.mf_growth_factor_for_lumpsum <- function(years, mf_growth) (1+(0.01*mf_growth))^years
-
-
 #' @title Monthly inestment for Financial Independence
 #' @description Get monthly investment needed into mutual funds as of now for achieving fin independence by a given period of time.
 #' Returns the monthly investment needed for the current year. And if sip_growth > 0, this means the monthly investment is planned to increase by the same every year (maybe due to more earning, inflation etc)
 #' @export
-monthly_investments_till_financial_independence <- function(current_monthly_expense, start_year, withdrawal_period, currency_inflation, lifestyle_inflation, mf_growth, sip_growth, life_expectancy, age_current) {
+monthly_investments_till_financial_independence <- function(current_monthly_expense, start_year, withdrawal_period,
+                                                            currency_inflation, lifestyle_inflation, mf_growth,
+                                                            sip_growth, life_expectancy, age_current) {
 
   lumpsum_needed = lumpsum_needed_for_ind(current_monthly_expense, start_year, withdrawal_period, currency_inflation, lifestyle_inflation, mf_growth, life_expectancy, age_current)
-  mf_growth_factor = .mf_growth_factor_for_sip(start_year, sip_growth, mf_growth)
+  mf_growth_factor = .mf_growth_factor_for_sip(mf_growth, start_year, sip_growth)
 
-  (lumpsum_needed/mf_growth_factor)/12
+  list(monthly_investment = (lumpsum_needed/mf_growth_factor)/12,
+       lumpsum_needed_at_beginning_of_start_year = lumpsum_needed)
 }
 
